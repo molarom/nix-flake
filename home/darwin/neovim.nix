4cdaae4764e71ca5b44e91ab5ea83e11bcff8c22
molarom <brandon.epperson@protonmail.com>
2024-07-02 18:27:40 -0400

    (mba) update: neovim config

--- home/darwin/neovim.nix
+++ home/darwin/neovim.nix
@@ -1,12 +1,9 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ../../modules/home/neovim/neovim.nix
   ];
 
   programs.neovim = {
     enable = true;
-    extraTSParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
-      ruby
-    ];
   };
 }
