969d0b217ff88530c188adf2b343a5f74a75d6c4
Brandon Epperson <brandon.epperson@protonmail.com>
2024-07-05 10:25:24 -0400

    (work) add: missing configuration

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -23,6 +23,19 @@
     ];
   };
 
+  services = {
+    nix-daemon = {
+      enable = true;
+    };
+  };
+
+  programs = {
+    # Create /etc/zshrc that loads the nix-darwin environment.
+    zsh = {
+      enable = true;
+    };
+  };
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
