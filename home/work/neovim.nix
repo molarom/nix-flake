f284fa8cdbb9fdfa466b9e3aa7be9a48bc787094
Brandon Epperson <brandon.epperson@protonmail.com>
2024-09-09 05:19:49 -0400

    (work) add: regal none-ls config

--- home/work/neovim.nix
+++ home/work/neovim.nix
@@ -29,6 +29,7 @@
       "null_ls.builtins.diagnostics.actionlint,"
       "null_ls.builtins.diagnostics.ansiblelint,"
       "null_ls.builtins.diagnostics.pylint,"
+      "null_ls.builtins.diagnostics.regal,"
       "null_ls.builtins.diagnostics.sqlfluff.with({ extra_args = {'--dialect', 'postgres', '-e', 'CP02,CP05,LT02,LT05,LT06,LT07,LT08,LT09,LT12'}}),"
       "null_ls.builtins.formatting.black,"
       "null_ls.builtins.formatting.pg_format,"
