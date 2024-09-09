c23c106abde7ac80865f40da8b85e39f4a332887
Brandon Epperson <brandon.epperson@protonmail.com>
2024-09-09 04:55:15 -0400

    fix: auto-optimise-store failing builds

--- settings/base/nix-settings.nix
+++ settings/base/nix-settings.nix
@@ -9,15 +9,21 @@
       };
     }
     else {dates = "weekly";};
+  # See: https://github.com/NixOS/nix/issues/7273
+  optimise_store =
+    if pkgs.system == "aarch64-darwin"
+    then {auto-optimise-store = false;}
+    else {auto-optimise-store = true;};
 in {
   nix = {
     package = pkgs.nix;
-    settings = {
-      # Enable Flakes
-      experimental-features = ["nix-command" "flakes"];
-      # Optimise nix store for every build.
-      auto-optimise-store = true;
-    };
+    settings =
+      {
+        # Enable Flakes
+        experimental-features = ["nix-command" "flakes"];
+        auto-optimise-store = true;
+      }
+      // optimise_store;
 
     # Setup automatic garbage collection.
     gc =
