ddc260761427d51500cd8c6680b61a435e99154d
Brandon Epperson <brandon.epperson@protonmail.com>
2024-09-09 04:40:57 -0400

    add: texlive

--- home/linux/neovim.nix
+++ home/linux/neovim.nix
@@ -1,9 +1,18 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ../../modules/home/neovim
   ];
 
   programs.neovim = {
     enable = true;
+    extraTSParsers = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      latex
+    ];
+    additionalPackages = with pkgs; [
+      texlab
+    ];
+    lspConfig = [
+      "lspconfig.texlab.setup{}"
+    ];
   };
 }
--- home/linux/packages.nix
+++ home/linux/packages.nix
@@ -7,6 +7,7 @@
       gnumake
       go
       spotify
+      texliveBasic
     ];
   };
 }
