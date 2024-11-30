613a9651413519a5395b95c683c982d23d6ecba5
molarom <brandon.epperson@protonmail.com>
2024-11-30 12:37:21 -0800

    (neovim) move: lsp autoformat fn outside of null-ls on_attach

--- modules/home/neovim/neovim.nix
+++ modules/home/neovim/neovim.nix
@@ -108,6 +108,23 @@ in {
         -- LSP configs
         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+        -- Autoformatting
+        autoformat = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({
+              group = augroup,
+              buffer = bufnr,
+            })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = bufnr })
+              end,
+            })
+          end
+        end
+
         return {
           {
             "nvimtools/none-ls.nvim",
@@ -125,21 +142,7 @@ in {
                   null_ls.builtins.formatting.alejandra,
                   ${(lib.concatStringsSep ",\n" cfg.nullLsSources)}
                 },
-                on_attach = function(client, bufnr)
-                  if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({
-                      group = augroup,
-                      buffer = bufnr,
-                    })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                      group = augroup,
-                      buffer = bufnr,
-                      callback = function()
-                        vim.lsp.buf.format({ bufnr = bufnr })
-                      end,
-                    })
-                  end
-                end
+                on_attach = autoformat
               })
             end,
           },
