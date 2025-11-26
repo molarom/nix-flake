5d30cf0d6513389d304676d1945b7b9170015f7d
Brandon Epperson <brandon.epperson@protonmail.com>
2025-11-26 14:16:25 -0600

    (darwin) update: texlive packages

--- home/darwin/packages.nix
+++ home/darwin/packages.nix
@@ -1,24 +1,28 @@
 {pkgs, ...}: {
   home = {
     packages = with pkgs; [
+      claude-code
       corepack
       cmake
       gcc
       gnumake
       go
       graphviz
-      texliveBasic
       nodejs_22
       go-tools
       govulncheck
       krew
       kubectl
       parallel
+      qpdf
       tree
       trivy
       unixtools.watch
       (pkgs.python313.withPackages (p: [
         p.pylatexenc # render-markdown.nvim
+      ]))
+      (pkgs.texliveBasic.withPackages (p: [
+        p.xcolor
       ]))
     ];
   };
